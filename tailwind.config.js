caee7aa5025ca33ad7f30cb652aab4ebd8bcea83
egileekada <egileoniso.ekada@gmail.com>
2022-05-18 21:46:48 +0100

    css update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: {
-    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
-    // These options are passed through directly to PurgeCSS
-  },
+  purge: [],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
